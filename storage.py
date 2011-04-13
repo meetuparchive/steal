07520f33a4fef7c4746be17c9dcbacd214f62415
Nathan Hamblen <nathan@technically.us>
2011-04-12 20:00:28 -0400

    insert events into sqlite db

--- storage.py
+++ storage.py
@@ -1,4 +1,18 @@
 import sqlite3
 
+conn = sqlite3.connect("meetups.sqlite")
+
+def setup():
+    with conn:
+        if not conn.execute(
+            "select name from sqlite_master where type='table' and name='event'"
+            ).fetchall():
+            conn.execute("create table event (id text, name text)")
+
+setup()
+
 def event_callback(event):
-    print event
+    with conn:
+        conn.execute("""insert into event values (?, ?)""",
+                  (event['id'], event['name']))
+        print("added %s" % event['name'])
