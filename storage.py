699e2950d8ad380922ce7388038fc5b91892f96d
Nathan Hamblen <nathan@technically.us>
2011-04-13 14:53:21 -0400

    event url, time

--- storage.py
+++ storage.py
@@ -4,13 +4,15 @@ conn = sqlite3.connect("meetups.sqlite")
 
 def setup():
     with conn:
-            conn.execute("create table if not exists event (id text, name text)")
-            conn.execute("create unique index if not exists event_id on event (id)")
+        conn.execute("""create table if not exists event 
+                        (id text, name text, description text, url text, time integer)""")
+        conn.execute("create unique index if not exists event_id on event (id)")
 
 setup()
 
 def event_callback(event):
     with conn:
-        conn.execute("""insert or replace into event values (?, ?)""",
-                  (event['id'], event['name']))
+        values = [event.get(k, None) for k in ['id', 'name', 'description', 'event_url', 'time']]
+        conn.execute("""insert or replace into event values
+                        (?, ?, ?, ?, ?)""", values)
         print("added %s" % event['name'])
