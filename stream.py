44653c41a1cfe2ff02f9166322251b536a837cf3
Nathan Hamblen <nathan@technically.us>
2011-04-13 18:13:46 -0400

    use array to avoid global

--- stream.py
+++ stream.py
@@ -1,16 +1,14 @@
 import pycurl, json
 
-last = ""
-
 def jsonizer(event_callback):
     """Passes json dicts to the given callback"""
+    last = [""]
     def consume(data):
         """Buffers data and invokes event_callback on completed lines"""
-        global last
-        lines = (last + data).split("\n")
+        lines = (last[0] + data).split("\n")
         for l in lines[:-1]:
             event_callback(json.loads(l))
-        last = lines[-1]
+        last[0] = lines[-1]
 
     conn = pycurl.Curl()
     conn.setopt(pycurl.URL, "http://stream.meetup.com/2/open_events")
